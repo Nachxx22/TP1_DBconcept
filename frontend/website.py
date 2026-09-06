cc2c82d62a9c84f00f5938b7d82aaff21e200e25
Maripazql27 <maripql3206@gmail.com>
2026-09-06 14:45:34 -0600

    Envio de datos usando post a flask, todo funcionó bien

--- frontend/website.py
+++ frontend/website.py
@@ -1,18 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request 
 
+#Se decidió trabajar con flask para crear la página web
 app = Flask(__name__)
 
-"Aquí se definen las rutas de la aplicación web"
+#Aquí se definen las rutas de la aplicación web
 
-"Llamada a la página 1 o principal, donde muestra tabla y boton"
+#"Llamada a la página 1 o principal, donde muestra tabla y boton"
 @app.route("/")
 def inicio():
     return render_template("empleados.html")
 
 
-"Llamada a la página 2 o insertar, donde esta el formulario para insertar un nuevo empleado"
-@app.route("/add")
+#Llamada a la página 2 o insertar, donde esta el formulario para insertar un nuevo empleado
+@app.route("/add", methods=["GET", "POST"])
 def add():
+    if request.method == "POST":
+        # Procesar los datos del formulario e imprimir para verificar que si se recibieron
+        nombre = request.form["nombre"]
+        salario = request.form["salario"]
+        print("Nombre:", nombre)
+        print("Salario:", salario)
     return render_template("add.html")
 
 if __name__ == "__main__":
