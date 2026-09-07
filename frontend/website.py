d61d31168b1d13b8ca48d0114fedfed471503521
Maripazql27 <maripql3206@gmail.com>
2026-09-06 19:17:11 -0600

    Mensajes de la capa lógica (flask/jinja)

--- frontend/website.py
+++ frontend/website.py
@@ -14,13 +14,17 @@ def inicio():
 #Llamada a la página 2 o insertar, donde esta el formulario para insertar un nuevo empleado
 @app.route("/add", methods=["GET", "POST"])
 def add():
+    mensaje = ""
+    #Si el método es POST, significa que se envió el formulario, entonces se procesan los datos
     if request.method == "POST":
         # Procesar los datos del formulario e imprimir para verificar que si se recibieron
         nombre = request.form["nombre"]
         salario = request.form["salario"]
         print("Nombre:", nombre)
         print("Salario:", salario)
-    return render_template("add.html")
+
+        mensaje = "Empleado agregado correctamente."
+    return render_template("add.html", mensaje=mensaje)
 
 if __name__ == "__main__":
     app.run(debug=True)
