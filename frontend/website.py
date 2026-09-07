b78daa7de5615759289c4e381674168b80afb939
Maripazql27 <maripql3206@gmail.com>
2026-09-07 15:02:16 -0600

    Agregar manejo de empleados en la página principal y diseño para no empleados

--- frontend/website.py
+++ frontend/website.py
@@ -1,35 +1,42 @@
 from flask import Flask, render_template, request 
-
 #Se decidió trabajar con flask para crear la página web
 app = Flask(__name__)
 
-#Aquí se definen las rutas de la aplicación web
 
+
+#Aquí se definen las rutas de la aplicación web
 #"Llamada a la página 1 o principal, donde muestra tabla y boton"
 @app.route("/")
 def inicio():
-    return render_template("empleados.html")
+    empleados = [
+        [1, "Juan Pérez", 2500],
+        [2, "María Gómez", 3000],
+        [3, "Carlos Rodríguez", 2800]
+    ]
+    return render_template("empleados.html", empleados=empleados)
+
+
 
 
 #Llamada a la página 2 o insertar, donde esta el formulario para insertar un nuevo empleado
 @app.route("/add", methods=["GET", "POST"])
 def add():
-    mensaje = ""
     #Si el método es POST, significa que se envió el formulario, entonces se procesan los datos
-    if request.method == "POST":
-        # Procesar los datos del formulario e imprimir para verificar que si se recibieron
+    if request.method == "POST":s
         nombre = request.form["nombre"]
         salario = request.form["salario"]
         print("Nombre:", nombre)
         print("Salario:", salario)
+    return render_template("add.html")
 
-        mensaje = "Empleado agregado correctamente."
-    return render_template("add.html", mensaje=mensaje)
+
 
 # Ruta temporal para probar la pantalla de éxito
 @app.route("/prueba-exito")
 def prueba_exito():
     return render_template("exito.html")
+
+
 
 
 # Ruta temporal para probar la pantalla de error
